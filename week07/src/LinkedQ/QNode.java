11b1180da75d8bc72179ff09c0623fa1786a75d7
Yeol <74747295+Ye0l@users.noreply.github.com>
2021-04-15 21:00:01 +0900

    Add files via upload

---
+++ week07/src/LinkedQ/QNode.java
@@ -0,0 +1,18 @@
+package LinkedQ;
+
+interface Queue {
+	boolean isEmpty();
+
+	void enQueue(char item);
+
+	char deQueue();
+
+	void delete();
+
+	char peek();
+}
+
+class QNode {
+	char data;
+	QNode link;
+}
